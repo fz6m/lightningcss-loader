ec88df8dad4a129fa2bf4ce4fa09f3fb2988362a
fz6m <59400654+fz6m@users.noreply.github.com>
2025-02-07 22:19:11 +0900

    chore: code format 🍭

--- src/loader.ts
+++ src/loader.ts
@@ -8,7 +8,7 @@ const LOADER_NAME = `lightningcss-loader`
 export async function LightningCssLoader(
   this: LoaderContext<ILightningCssLoaderConfig>,
   source: string,
-  prevMap?: Record<string, any>
+  prevMap?: Record<string, any>,
 ): Promise<void> {
   const done = this.async()
   const options = this.getOptions()
@@ -17,8 +17,8 @@ export async function LightningCssLoader(
   if (implementation && typeof implementation.transform !== 'function') {
     done(
       new TypeError(
-        `[${LOADER_NAME}]: options.implementation.transform must be an 'lightningcss' transform function. Received ${typeof implementation.transform}`
-      )
+        `[${LOADER_NAME}]: options.implementation.transform must be an 'lightningcss' transform function. Received ${typeof implementation.transform}`,
+      ),
     )
     return
   }
